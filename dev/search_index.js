39768aa9e1f582226b0f66df366876a9ead3c9b6
Documenter.jl <documenter@juliadocs.github.io>
2021-02-18 13:49:33 +0000

    build based on bf0504f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ELM","category":"page"},{"location":"#ELM","page":"Home","title":"ELM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ELM]","category":"page"},{"location":"#ELM.ExtremeLearningMachine","page":"Home","title":"ELM.ExtremeLearningMachine","text":"ExtremeLearningMachine(n_hidden_neurons::Integer; C = 100)\n\nCreates an ELM.\n\n\n\n\n\n","category":"type"}]
+}
