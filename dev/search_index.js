f51454bd7887dcc73a638eb99fe4c4a568e878fc
Documenter.jl <documenter@juliadocs.github.io>
2021-02-20 12:51:43 +0000

    build based on 21d6da2

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExtremeLearning","category":"page"},{"location":"#ExtremeLearning","page":"Home","title":"ExtremeLearning","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExtremeLearning]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExtremeLearning","category":"page"},{"location":"#ExtremeLearning","page":"Home","title":"ExtremeLearning","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExtremeLearning]","category":"page"},{"location":"#ExtremeLearning.ELM","page":"Home","title":"ExtremeLearning.ELM","text":"\n\n\n\n","category":"type"}]
 }
