663fd82e39a303bf6d7d0d7f0c93d39de9b1d57c
Gabriel Baraldi <baraldi@usp.br>
2021-02-20 09:49:52 -0300

    Adding tests

--- src/base.jl
+++ src/base.jl
@@ -2,23 +2,22 @@
 # -------------------
 abstract type AbstractELM end
 
+function sigmoid(x)
+    # Sigmoid activation
+    1 ./ (1 + exp(-x))
+end
+"""
+
+
+"""
 struct ELM{T,I, S<:AbstractArray{T}, F} <: AbstractELM
-    # Extreme Learning Machine
-    #
-    # Properties
-    # ----------
-    # `hidden_layer` is the hidden layer inside this ELM
-    # `output_weights` is the weight matrix calculated during training
-    # `betais the regularisation parameter that improves generalisation (not implemented as of now)
-    
+
     n_hidden_neurons::I
     weight_matrix::S
     bias_vector::S
     activation::F
     output_weights::S
     β::T 
-	
-
 end
 
 
@@ -26,26 +25,24 @@ function ELM(n_hidden_neurons::Int,
         input_data::AbstractArray{T},
         output_data::AbstractArray{T};
         activation::Function = sigmoid,
-        reg::T = 0.0) where T<:AbstractFloat
+        regularization::T = zero(T)) where T<:AbstractFloat
 
     in_size = size(input_data, 1)
     out_size = size(output_data, 1)
-    weight_matrix = rand(n_hidden_neurons, in_size) .* 2 .- 1
-    output_weights = zeros(out_size,n_hidden_neurons)
-    bias_vector = rand(n_hidden_neurons,1)
-    return ELM{T,typeof(n_hidden_neurons), typeof(input_data),typeof(activation)}(n_hidden_neurons,
+    weight_matrix = rand(T,n_hidden_neurons, in_size) .* 2 .- 1
+    output_weights = zeros(T,out_size,n_hidden_neurons)
+    bias_vector = rand(T,n_hidden_neurons,1)
+    return ELM{T,typeof(n_hidden_neurons), 
+            typeof(weight_matrix), 
+            typeof(activation)
+            }(n_hidden_neurons,
             weight_matrix,
             bias_vector, 
             activation,
-            output_weights,reg)
+            output_weights,
+            regularization)
 
 end
-
-function sigmoid(x)
-    # Sigmoid activation
-    1 ./ (1 + exp(-x))
-end
-
 
 function fit!(elm::AbstractELM,
             x::AbstractArray{T},
